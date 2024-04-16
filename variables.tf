0449af4cfa249d48915e4f90ad1b201bf361c785
Ravishankar Thevar <40117131+RavishankarThavam@users.noreply.github.com>
2024-04-16 09:08:57 +0000

    AI suggested

--- variables.tf
+++ variables.tf
@@ -7,37 +7,71 @@
 variable "Application" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.Application != ""
+    error_message = "The Application must not be empty."
+  }
 }
 
 variable "Division" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.Division != ""
+    error_message = "The Division must not be empty."
+  }
 }
 
 variable "Function" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.Function != ""
+    error_message = "The Function must not be empty."
+  }
 }
 
 variable "Environment" {
   default = ""
   nullable = false
-}
 
+  validation {
+    condition     = var.Environment != ""
+    error_message = "The Environment must not be empty."
+  }
+}
 
 variable "L1_Technical_Owner" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.L1_Technical_Owner != ""
+    error_message = "The L1_Technical_Owner must not be empty."
+  }
 }
 
 variable "L2_Technical_Owner" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.L2_Technical_Owner != ""
+    error_message = "The L2_Technical_Owner must not be empty."
+  }
 }
 
 variable "L3_IT_Owner" {
   default = ""
   nullable = false
+
+  validation {
+    condition     = var.L3_IT_Owner != ""
+    error_message = "The L3_IT_Owner must not be empty."
+  }
 }
 
 variable "ITLT_Owner" {
@@ -60,6 +94,7 @@ variable "ITLT_Owner" {
       "onno.schippers@iff.com"
     ], var.ITLT_Owner)
     error_message = "The ITLT_Owner must be one of the predefined email addresses."
+  }
   }
 }
 
